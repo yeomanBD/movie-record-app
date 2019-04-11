cc287808aad684d111ffd98c573a59ccadeb7821
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2019-04-12 00:41:22 +0600

    add skeleton project for movie-record-app

---
+++ src/components/MovieTable.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MovieTable extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello from movie table</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieTable;
